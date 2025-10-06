44db7c90be7329765ecf7515b7cea9ab22cbe593
buxeyj <buxeyjones9@gmail.com>
2025-10-06 19:55:07 +0100

    Add fully working Neovim setup (Lazy.nvim + zenkai)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,24 @@
+-- Make sure Neovim can find your config inside dotfiles
+vim.opt.runtimepath:prepend(vim.fn.expand("~/dotfiles/.config/nvim"))
+
+-- Make sure Lazy.nvim’s path is in runtimepath
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Try loading Lazy.nvim explicitly
+pcall(require, "lazy")
+
+-- Load your actual Neovim config
 require("zenkai.core")
 require("zenkai.lazy")
+
--- .config/nvim/lua/zenkai/lazy.lua
+++ .config/nvim/lua/zenkai/lazy.lua
@@ -11,12 +11,11 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "zenkai.plugins" }, { import = "zenkai.plugins.lsp" } }, {
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    notify = false,
-  },
+require("lazy").setup({
+  { import = "zenkai.plugins" },
+  { import = "zenkai.plugins.lsp" },
+}, {
+  checker = { enabled = true, notify = false },
+  change_detection = { notify = false },
 })
+
